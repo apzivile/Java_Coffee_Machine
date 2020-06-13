3cd4401e26ef55f4a5ef470e809e2b9a487bed0c
apzivile <zivile.apo@gmail.com>
2020-06-13 16:16:38 +0100

    coffee machine project array topic problems

---
+++ Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long[] longNumbers = { 100000000001L, 100000000002L, 100000000003L };
+        char[] array = new char[-1];
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
